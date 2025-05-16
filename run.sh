9c6665a22f2333e9b5fcf5259474fb2c75479edb
Aaron <wunderleaaron@gmail.com>
2025-05-16 19:11:23 +0200

    edited run sh so maybe correct error codes are displayed now in crow

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env sh
 BIN_DIR="$(dirname "$0")/build/install/compiler/bin"
-$BIN_DIR/compiler "$@"
+$BIN_DIR/compiler "$1" "out1.s"
+echo "Exit Code: $?"
 set -e
-as -o out1.o "$2"
+as -o out1.o "out1.s"
 ld -o "$2" out1.o
+$BIN_DIR/compiler "$1" "out1.s"
